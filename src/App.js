fc127aeca52917f43238b5bdab1da58595f7bcd0
sjpark <sorajin485@naver.com>
2020-11-24 02:49:51 +0900

    materializercss 적용

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#" class="brand-logo">Logo</a>
+          <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+          </ul>
+        </div>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
