696cacfc6006f208c9a0528f6f6bcfcc599e9ab7
sjpark <sorajin485@naver.com>
2020-11-25 02:38:10 +0900

    초기 템플릿 작성중 css 적용중

--- src/App.js
+++ src/App.js
@@ -1,35 +1,122 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from  'materialize-css/dist/js/materialize.min.js';
+class App extends Component {
+  componentDidMount() {
+    let sidenav = document.querySelector('#mobile-demo');
+    M.Sidenav.init(sidenav, {});
+  }
+  render(){
 
-function App() {
   return (
     <div className="App">
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo">Logo</a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
+      {/* NAV */}
+      <nav className="nav-extended">
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">Logo</a>
+          <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a href="sass.html">Sass</a></li>
             <li><a href="badges.html">Components</a></li>
             <li><a href="collapsible.html">JavaScript</a></li>
           </ul>
         </div>
+        <div className="nav-content">
+          <ul className="tabs tabs-transparent">
+            <li className="tab"><a href="#test1">매장소개</a></li>
+            <li className="tab"><a href="#test2">고기구경</a></li>
+            <li className="tab"><a href="#test3">매장리뷰</a></li>
+            <li className="tab"><a href="#test4">오시는길</a></li>
+          </ul>
+        </div>
       </nav>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <ul className="sidenav" id="mobile-demo">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+      {/* 인트로 배너 */}
+      <div id="index-banner" className="parallax-container">
+        <div className="section no-pad-bot">
+          <div className="container">
+            <br></br>
+            <h1 className="header center teal-text text-lighten-2">Parallax Template</h1>
+            <div className="row center">
+              <h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+            </div>
+            <div className="row center">
+              <a href="http://materializecss.com/getting-started.html" id="download-button" className="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+            </div>
+            <br></br>
+
+          </div>
+        </div>
+        <div className="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"/></div>
+      </div>
+
+      <div className="container">
+        <div className="section">
+          {/*   Icon Section   */}
+          <div className="row">
+            <div className="col s12 m4">
+              <div className="icon-block">
+                <h2 className="center brown-text"><i className="material-icons">flash_on</i></h2>
+                <h5 className="center">Speeds up development</h5>
+
+                <p className="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+              </div>
+            </div>
+
+            <div className="col s12 m4">
+              <div className="icon-block">
+                <h2 className="center brown-text"><i className="material-icons">group</i></h2>
+                <h5 className="center">User Experience Focused</h5>
+
+                <p className="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+              </div>
+            </div>
+
+            <div className="col s12 m4">
+              <div className="icon-block">
+                <h2 className="center brown-text"><i className="material-icons">settings</i></h2>
+                <h5 className="center">Easy to work with</h5>
+
+                <p className="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* 푸터 */}
+      <footer className="page-footer">
+          <div className="container">
+            <div className="row">
+              <div className="col l6 s12">
+                <h5 className="white-text">Footer Content</h5>
+                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div className="col l4 offset-l2 s12">
+                <h5 className="white-text">Links</h5>
+                <ul>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="footer-copyright">
+            <div className="container">
+            © 2014 Copyright Text
+            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+      </footer>
     </div>
   );
-}
+}}
 
 export default App;
